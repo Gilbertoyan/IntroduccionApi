0e2a0052ede3aa2f61702f95177fcafea51f4ebc
Gilbertoyan <gilbertoyan7@gmail.com>
2022-05-10 12:01:07 -0600

    adios mundo

--- Proyecto express/app.js
+++ Proyecto express/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/adios', function(req, res) {
     res.send('hola mundo!');
+});
+app.get('/adios', function(req, res) {
+    res.send('adios mundo!');
 });
 
 app.listen(3000, function(){
