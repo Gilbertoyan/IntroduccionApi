518357c3d8776595c0fefdc712f2176ab4b8d9e8
Gilbertoyan <gilbertoyan7@gmail.com>
2022-05-11 11:57:37 -0600

    Se mostrara solo un usuario por id

--- Proyecto express/routes/routes.js
+++ Proyecto express/routes/routes.js
@@ -16,6 +16,17 @@ app.get('/users',(request, response) => {
 });
 
 
+//mostrar solo un usuario por id
+app.get('/users/:id', (request, response) => { 
+    const id= request.params.id;
+
+    pool.query('SELECT * FROM users WHERE id = ? ', id, (error, result) => { 
+        if(error) throw error;
+
+        response.send(result);
+    });
+});
+
 
 
 }
