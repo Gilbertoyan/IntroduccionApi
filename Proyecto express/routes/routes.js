b9d52fe7f3eb99832dff9bfaa7796ff6f0204c6f
Gilbertoyan <gilbertoyan7@gmail.com>
2022-05-12 11:19:16 -0600

    Conexion a users

--- Proyecto express/routes/routes.js
+++ Proyecto express/routes/routes.js
@@ -27,6 +27,18 @@ app.get('/users/:id', (request, response) => {
     });
 });
 
+//agregar un nuevo usuario
+app.post('/users', (request, response) => { 
+   
+
+    pool.query('INSERT INTO users SET ? ', request.body,(error, result) => { 
+        if(error) throw error;
+
+        response.status(201).send(`User added with ID: ${result,insertId}`);
+    });
+});
+
+
 
 
 }
