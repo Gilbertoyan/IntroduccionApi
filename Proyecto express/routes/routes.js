a9c45338225f5a83f63bbeadaab677e70a884d1b
Gilbertoyan <gilbertoyan7@gmail.com>
2022-05-12 11:25:26 -0600

    Actualizamos el usuario existente

--- Proyecto express/routes/routes.js
+++ Proyecto express/routes/routes.js
@@ -38,6 +38,19 @@ app.post('/users', (request, response) => {
     });
 });
 
+//actualizar un usuario existente
+app.put('/users/:id', (request, response) => { 
+    const id= request.params.id;
+
+    pool.query('UPDATE users SET ? WHERE id = ? ', [request.body, id], id, (error, result) => { 
+        if(error) throw error;
+
+        response.send('User updated successfully');
+    });
+});
+
+
+
 
 
 
